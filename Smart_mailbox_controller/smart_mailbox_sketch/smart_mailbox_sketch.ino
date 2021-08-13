edab4b234d8a7d7a2c8020c0a579f3301ae227c6
JasonHewitt86 <85660980+JasonHewitt86@users.noreply.github.com>
2021-08-14 09:05:42 +1000

    Added humidity feedback to web server

--- Smart_mailbox_controller/smart_mailbox_sketch/smart_mailbox_sketch.ino
+++ Smart_mailbox_controller/smart_mailbox_sketch/smart_mailbox_sketch.ino
@@ -7,7 +7,7 @@
 const char* ssid = "";
 const char* password = "";
 
-const unsigned long eventInterval = 10000;
+const unsigned long eventInterval = 60000;
 unsigned long previousTime = 0;
 
 #define DHTPIN 16
@@ -16,16 +16,9 @@ unsigned long previousTime = 0;
 //Type of sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-int    HTTP_PORT   = 80;
-String HTTP_METHOD = "POST"; // or "POST"
-char   HOST_NAME[] = ""; // hostname of web server:
-String PATH_NAME   = "";
-
 WiFiClient client;
 
 String readDHTTemperature() {
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
   if (isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -38,14 +31,12 @@ String readDHTTemperature() {
 }
 
 String readDHTHumidity() {
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   if (isnan(h)) {
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
   else {
-    Serial.println(h);
     return String(h);
   }
 }
@@ -69,17 +60,41 @@ void setup() {
 
   dht.begin();
   wifiConnect();
-  curlRequest();
+  tempCurlRequest();
 }
 
-void curlRequest() {
+void tempCurlRequest() {
 
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     HTTPClient http;
     String dhtT = readDHTTemperature();
     String json = "{\"temp\":\""+dhtT+"\"}";
     http.begin("http://"); //Specify destination for HTTP request
-    http.addHeader("Authorization", "Token ");
+    http.addHeader("Authorization", "Token");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(json);
+    //int httpCode =http.GET();
+    if (httpCode > 0) {
+      Serial.print("Http response code: ");
+      Serial.println(httpCode);
+      Serial.println(json);
+      //String payload = http.getString();
+      //Serial.println(payload);
+      if (httpCode == 201) {
+        Serial.println("Success!");
+      }
+    }
+  }
+}
+
+void humidCurlRequest() {
+
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+    HTTPClient http;
+    String dhtH = readDHTHumidity();
+    String json = "{\"humidity\":\""+dhtH+"\"}";
+    http.begin("http://"); //Specify destination for HTTP request
+    http.addHeader("Authorization", "Token");
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.POST(json);
     //int httpCode =http.GET();
@@ -100,7 +115,8 @@ void loop() {
 
   unsigned long currentTime = millis();
   if( currentTime - previousTime >= eventInterval){
-    curlRequest();
+    tempCurlRequest();
+    humidCurlRequest();
     previousTime = currentTime;
     
   }
